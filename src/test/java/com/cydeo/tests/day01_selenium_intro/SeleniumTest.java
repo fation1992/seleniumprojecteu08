8efdfe9c1ba6a5adf6cd837b8e10b58389b1aa3b
fation1992 <fationshaliu@gmail.com>
2022-04-27 17:10:29 +0200

    creating first selenium project

---
+++ src/test/java/com/cydeo/tests/day01_selenium_intro/SeleniumTest.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day01_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+        // 1- Setting up the web driver
+        WebDriverManager.chromedriver().setup();
+        // 2- Creating an instances for chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        // 3- test if driver is working as expected
+        driver.get("https://www.google.com");
+
+    }
+}
