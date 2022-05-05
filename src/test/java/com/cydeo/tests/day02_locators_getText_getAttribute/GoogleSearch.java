0177d5450b5e5733cc368e5577b59e9867a090e8
fation1992 <fationshaliu@gmail.com>
2022-05-05 17:34:34 +0200

    creating Day02 find Elements By link

---
+++ src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/GoogleSearch.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.day02_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://www.google.com");
+
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
+        String expectedInTitle = "apple";
+        String currentTitle = driver.getTitle();
+
+        if (currentTitle.contains(expectedInTitle)){
+            System.out.println("passed");
+        }else
+            throw new RuntimeException("error, title failed");
+
+        driver.close();
+    }
+}
