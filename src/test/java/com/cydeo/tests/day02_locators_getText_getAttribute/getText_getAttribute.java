12c324cd770f53be2b9a2f82afb346275fbdbaa4
fation1992 <fationshaliu@gmail.com>
2022-05-05 19:49:24 +0200

    creating getText_getAttribute

--- src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/getText_getAttribute.java
@@ -1,8 +1,19 @@
 package com.cydeo.tests.day02_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class getText_getAttribute {
 
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://practice.cydeo.com/registration_form");
+
 
 
     }
