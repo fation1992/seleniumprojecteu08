c4a1d9c6e92a3ed2ff88a0cefea4d1c07dd57678
fation1992 <fationshaliu@gmail.com>
2022-05-05 19:46:01 +0200

    creating LibraryLoginPage

--- src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day02_locators_getText_getAttribute/LibraryLoginPage.java
@@ -1,13 +1,15 @@
 package com.cydeo.tests.day02_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LibraryLoginPage {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -15,6 +17,17 @@ public class LibraryLoginPage {
 
         driver.get("https://library2.cybertekschool.com/login.html");
 
+        Thread.sleep(4000);
+
+        WebElement usernameInput = driver.findElement(By.className("form-control"));
+        usernameInput.sendKeys("username@email.com");
+
+
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect");
+
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
 
     }
 }
