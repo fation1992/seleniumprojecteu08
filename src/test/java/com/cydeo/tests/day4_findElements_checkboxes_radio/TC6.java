e799b7c98ea51a89734c919dfb8f6bd8c9e5cef5
fation1992 <fationshaliu@gmail.com>
2022-05-19 14:31:55 +0200

    day 04 added tc5

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/TC6.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radio;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class TC6 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/abtest");
+
+        WebElement cydeoLink = driver.findElement(By.linkText("CYDEO"));
+
+        System.out.println(cydeoLink.isDisplayed());
+
+        driver.navigate().refresh();
+
+        cydeoLink= driver.findElement(By.linkText("CYDEO"));
+
+        System.out.println(cydeoLink.isDisplayed());
+
+
+
+    }
+}
+/*
+TC #6: StaleElementReferenceException Task
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/abtest
+3- Locate “CYDEO” link, verify it is displayed.
+4- Refresh the page.
+5- Verify it is displayed, again.
+This is a simple StaleElementReferenceException to understand what is
+this exception and how to handle it.
+ */
