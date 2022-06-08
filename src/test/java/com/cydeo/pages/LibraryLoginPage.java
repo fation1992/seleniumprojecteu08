c5feafd03e1680cf934323e1863930eba1f93ca4
fation1992 <fationshaliu@gmail.com>
2022-06-08 13:57:16 +0200

    LibraryLoginPage.java

---
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -0,0 +1,13 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+
+
+    public  LibraryLoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
